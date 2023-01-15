bcdd74bf4a94c8002126ddb9114937385e396c76
pravinpk9 <83226357+pravinpk9@users.noreply.github.com>
2023-01-15 17:45:59 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+import openai
+openai.api_key = "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"
+
+# Define a flask app
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+try:
+  def model_predict():
+    response = openai.Completion.create(
+      model="text-davinci-003",
+      prompt="what is acetaminophen  used for?\nA:",
+      temperature=0,
+      max_tokens=100,
+      top_p=1,
+      frequency_penalty=0.0,
+      presence_penalty=0.0,
+      stop=["\n"]
+    )
+    return response
+except Exception as err:
+  return: 'error for: '+ str(err)
